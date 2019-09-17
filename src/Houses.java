7db20bb546c31972bd84dcf2ab1fce91b7238451
538751 <538751@0338S-S-R0B6H0F.sdcs.local>
2019-09-17 14:24:08 -0700

    houses

---
+++ src/Houses.java
@@ -0,0 +1,18 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+public static void main(String[] args) {
+	Robot r2d2 =  new Robot();
+	r2d2.setX(20);
+	r2d2.setY(550);	
+	r2d2.penDown();
+	r2d2.move(100);
+	r2d2.turn(90);
+	r2d2.move(20);
+	r2d2.turn(90);
+	r2d2.move(100);
+	
+	
+	
+}
+}
