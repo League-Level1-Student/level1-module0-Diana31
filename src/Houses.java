8f5cd2b8c212fd8721b2d2f46f5ff9ae5e3a14b4
538751 <538751@0338S-S-R0B6H0F.sdcs.local>
2019-09-24 14:23:39 -0700

    houses

--- src/Houses.java
+++ src/Houses.java
@@ -1,18 +1,27 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
+	static Robot r2d2 =  new Robot();
 public static void main(String[] args) {
-	Robot r2d2 =  new Robot();
 	r2d2.setX(20);
 	r2d2.setY(550);	
+	r2d2.setSpeed(10);		
 	r2d2.penDown();
-	r2d2.move(100);
+	for (int i = 0; i < 10; i++) {
+		drawHouse(100);
+	}
+}
+
+public static void drawHouse(int Height) {
+	r2d2.move(Height);
 	r2d2.turn(90);
 	r2d2.move(20);
 	r2d2.turn(90);
-	r2d2.move(100);
-	static void 
-	
-	
+	r2d2.move(Height); 
+	r2d2.turn(270);
+	r2d2.setPenColor(0,250,0);
+	r2d2.move(30);
+	r2d2.setPenColor(100,100,100);
+	r2d2.turn(270);
 }
 }
