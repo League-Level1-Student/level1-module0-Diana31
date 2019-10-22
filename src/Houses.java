3ab1a9d7eb3029f4d990aef8536d04a34553bcef
538751 <538751@0338S-Z-90TDE0P.sdcs.local>
2019-10-22 14:11:17 -0700

    Houses complete

--- src/Houses.java
+++ src/Houses.java
@@ -5,47 +5,48 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	static Robot r2d2 =  new Robot();
 public static void main(String[] args) {
-	
 	r2d2.setX(20);
 	r2d2.setY(550);	
 	r2d2.setSpeed(10);		
 	r2d2.penDown();
-		drawHouse("large");
-		drawHouse("small");
-		drawHouse("medium");
-		drawHouse("small");
-		drawHouse("large");
+	drawHouse("medium");
+	
 		
 }
+
 public static void drawHouse(String heightString) {
+	String bob = JOptionPane.showInputDialog("What size?");
 	int height = 0;
-	if(heightString.equals("small")) {
+	if(bob.equals("small")) {
 		height = 60;
+		r2d2.move(height);
+		drawPointyRoof();
+		
 	} 
-	if(heightString.equals("meduim")) {
+	if(bob.equals("meduim")) {
 		height = 120;
+		r2d2.move(height);
+		drawPointyRoof();
 	}
-	if(heightString.equals("large")) {
+	if(bob.equals("large")) {
 		height = 250;
+		r2d2.move(height);
 		drawFlatRoof();
 	}
-	r2d2.move(height);
-	r2d2.turn(90);
-	r2d2.move(20);
-	r2d2.turn(90);
-	r2d2.move(height); 
-	r2d2.turn(270);
-	r2d2.setPenColor(0,250,0);
-	r2d2.move(30);
-	r2d2.setPenColor(100,100,100);
-	r2d2.turn(270);
+	//r2d2.move(height);
+	//r2d2.turn(90);
+	//r2d2.move(20);
+	//r2d2.turn(90);
+	//r2d2.move(height); 
+	//r2d2.turn(270);
+	//r2d2.move(30);
+	//r2d2.turn(270);
 }
 public static void drawPointyRoof() {
 	r2d2.turn(45);
 	r2d2.move(25);
 	r2d2.turn(90);
-	r2d2.move(25);
-	r2d2.turn(45);
+	
 }
 public static void drawFlatRoof() {
 	r2d2.turn(90);
