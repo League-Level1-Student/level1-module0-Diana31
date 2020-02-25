23907eaab50553b30a46f92979db3ed3730289a7
538751 <538751@0338S-S-R0B6H0L.sdcs.local>
2020-02-25 14:24:28 -0800

    incomplete

---
+++ src/JackInTheBox.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class JackintheBox implements ActionListener {
+	private int clicked = 0;
+	public void Jack() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JButton button = new JButton();
+		button.setText("Surprise!!!");
+			frame.add(button);
+			frame.pack();
+			button.addActionListener(this);
+			
+			
+		}
---
+++ src/JackInTheBoxRunner.java
@@ -0,0 +1,5 @@
+public class JackInTheBoxRunner {
+	public static void main (String [] args) {
+		 new JackintheBox().Jack();
+	}
+}
